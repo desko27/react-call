f905a0b9f539e1b03089d965e54261868019e529
Ismael Ramon <desko27@gmail.com>
2024-08-19 14:51:10 +0200

    Pass lib tsconfigPath to vite dts plugin

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     react(),
     nodeExternals(),
     tsconfigPaths(),
-    dts({ include: ['lib'], rollupTypes: true }),
+    dts({ rollupTypes: true, tsconfigPath: 'tsconfig.lib.json' }),
   ],
   build: {
     copyPublicDir: false,
