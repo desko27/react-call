94d1a547c5eb619e385184242b8692ac738598dd
Ismael Ramon <desko27@gmail.com>
2024-08-19 16:00:58 +0200

    Reduce some bytes from the createCallable return structure

--- lib/createCallable/index.tsx
+++ lib/createCallable/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from 'react'
 import type {
-  CallFunction,
   UserComponent as UserComponentType,
   PrivateStackState,
   PrivateStackStateSetter,
@@ -13,45 +12,44 @@ export function createCallable<Props = void, Response = void, RootProps = {}>(
   let $setStack: PrivateStackStateSetter<Props, Response> | null = null
   let $nextKey = 0
 
-  const call: CallFunction<Props, Response> = (props) => {
-    if ($setStack === null) throw new Error('No <Root> found!')
+  return {
+    call: (props) => {
+      if ($setStack === null) throw new Error('No <Root> found!')
 
-    const key = String($nextKey++)
-    const promise = Promise.withResolvers<Response>()
+      const key = String($nextKey++)
+      const promise = Promise.withResolvers<Response>()
 
-    const end = (response: Response) => {
-      if ($setStack === null) return
-      promise.resolve(response)
-      $setStack((prev) => prev.filter((c) => c.key !== key))
-    }
-
-    const nextCall = { key, props, end }
-    $setStack((prev) => [...prev, nextCall])
-
-    return promise.promise
-  }
-
-  function Root(rootProps: RootProps) {
-    const [stack, setStack] = useState<PrivateStackState<Props, Response>>([])
-
-    useEffect(() => {
-      if ($setStack !== null)
-        throw new Error('Multiple instances of <Root> found!')
-      $setStack = setStack
-      return () => {
-        $setStack = null
-        $nextKey = 0
+      const end = (response: Response) => {
+        if ($setStack === null) return
+        promise.resolve(response)
+        $setStack((prev) => prev.filter((c) => c.key !== key))
       }
-    }, [])
 
-    return stack.map(({ props, ...call }) => (
-      <UserComponent
-        {...props}
-        key={call.key}
-        call={{ ...call, root: rootProps }}
-      />
-    ))
+      const nextCall = { key, props, end }
+      $setStack((prev) => [...prev, nextCall])
+
+      return promise.promise
+    },
+    Root: (rootProps: RootProps) => {
+      const [stack, setStack] = useState<PrivateStackState<Props, Response>>([])
+
+      useEffect(() => {
+        if ($setStack !== null)
+          throw new Error('Multiple instances of <Root> found!')
+        $setStack = setStack
+        return () => {
+          $setStack = null
+          $nextKey = 0
+        }
+      }, [])
+
+      return stack.map(({ props, ...call }) => (
+        <UserComponent
+          {...props}
+          key={call.key}
+          call={{ ...call, root: rootProps }}
+        />
+      ))
+    },
   }
-
-  return { Root, call }
 }
