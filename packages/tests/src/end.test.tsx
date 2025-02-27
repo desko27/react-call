d8abccd8eb9206966de6014da1ab72188f1e0f8d
Ismael Ramon <desko27@gmail.com>
2025-02-27 19:44:15 +0100

    Add tests to end()

--- packages/tests/src/end.test.tsx
+++ packages/tests/src/end.test.tsx
@@ -1,14 +1,39 @@
-import { describe, test } from 'vitest'
-// import { render } from 'vitest-browser-react'
-// import { Confirm } from './shared/Confirm'
+import { describe, expect, test } from 'vitest'
+import { render } from 'vitest-browser-react'
+import { Confirm } from './shared/Confirm'
 
-describe.todo('end()', () => {
+describe('end()', () => {
   describe('inside', () => {
-    test('removes target')
-    test('removes all')
+    test('removes target', async () => {
+      const screen = render(<Confirm.Root />)
+      Confirm.call({ message: 'foo' })
+      await screen.getByRole('button', { name: /no/i }).click()
+      await expect.element(screen.getByRole('dialog')).not.toBeInTheDocument()
+    })
+    test('removes all', async () => {
+      const screen = render(<Confirm.Root />)
+      const messages = ['foo', 'bar', 'xyz', '123', '456']
+      messages.forEach((message) => Confirm.call({ message }))
+      await screen
+        .getByRole('button', { name: /no/i })
+        .all()
+        .map((button) => button.click())
+      await expect.element(screen.getByRole('dialog')).not.toBeInTheDocument()
+    })
   })
   describe('outside', () => {
-    test('removes target')
-    test('removes all')
+    test('removes target', async () => {
+      const screen = render(<Confirm.Root />)
+      const promise = Confirm.call({ message: 'foo' })
+      Confirm.end(promise, false)
+      await expect.element(screen.getByRole('dialog')).not.toBeInTheDocument()
+    })
+    test('removes all', async () => {
+      const screen = render(<Confirm.Root />)
+      const messages = ['foo', 'bar', 'xyz', '123', '456']
+      messages.forEach((message) => Confirm.call({ message }))
+      Confirm.end(false)
+      await expect.element(screen.getByRole('dialog')).not.toBeInTheDocument()
+    })
   })
 })
